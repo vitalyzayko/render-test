30d05847ed92bf00a211b3907e083d97ed207cf6
z0048hzr <vitaly.zayko@siemens-energy.com>
2025-12-22 10:18:06 +0100

    commiting loearning 3 notebook backend

---
+++ index.js
@@ -0,0 +1,111 @@
+//const http = require('http')
+
+const express = require('express')
+const app = express()
+const cors = require("cors")
+
+app.use(express.json())
+
+app.use(cors())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  },
+  {    id: "4",
+    content: "Express is a framework for building web applications in Node.js",
+    important: true
+  },
+  {    id: "5",
+    content: "JavaScript is single-threaded",
+    important: false
+  },
+  {    id: "6",
+    content: "TypeScript is a superset of JavaScript",
+    important: true
+  },
+  {    id: "7",
+    content: "React is a library for building user interfaces",
+    important: true
+  }
+
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxID = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+    return String(maxID + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing"
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+//const PORT = 3001
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
