d58d13e5ebb63ddc7af14511924f7b819ca25260
z0048hzr <vitaly.zayko@siemens-energy.com>
2025-12-23 21:30:37 +0100

    final updates befor exercises 3.9

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const express = require('express')
 const app = express()
-const cors = require("cors")
+//const cors = require("cors")
 
 app.use(express.static('dist'))
 
 app.use(express.json())
 
-app.use(cors())
+//app.use(cors())
 
 let notes = [
   {
